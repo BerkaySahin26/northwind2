759273d4c238dd4071a9f051747c139ef6cf00ec
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-09-27 21:17:05 +0300

    Api

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,28 +1,47 @@
+import { ProductResponseModel } from './../../models/productResponseModel';
 import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSelectModule } from "@angular/material/select";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms'; // NgFormModule buradan import ediliyor
 import { CommonModule } from '@angular/common';
-
+import { HttpClient } from '@angular/common/http';
+import { response } from 'express';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [FormsModule, MatCardModule, MatButtonModule, MatToolbarModule, MatSelectModule, RouterOutlet, CommonModule,],
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSelectModule,
+    RouterOutlet,
+    CommonModule,
+    HttpClientModule,
+  ],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  apiUrl = 'https://localhost:7131/api/Products/getall';
 
-products:Product[] = [];
-constructor(){}
+  constructor(private httpClient: HttpClient) {}
 
-
-ngOnInit(): void {
-}
-
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data
+      });
+  }
 }
