c904276ecf5f64aa9a45ca63fca30d19c6b77b7d
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-10-14 21:51:21 +0300

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { CategoryComponent } from './components/category/category.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: ProductComponent },
   { path: 'products', component: ProductComponent },
   { path:"products/category/:categoryId",component:ProductComponent}, // CategoryComponent'e yönlendirme
   {path: "products/add",component:ProductAddComponent},
+  {path: "login",component:LoginComponent}
 ];
 
 @NgModule({
