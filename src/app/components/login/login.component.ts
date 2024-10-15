4d1efa42044b6c8cefff0b9aedd20fa4464daf2f
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-10-15 18:52:50 +0300

    Login uptdate

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup,FormBuilder,FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { response } from 'express';
+import { MatSelectModule } from '@angular/material/select';
+import { AuthService } from '../../services/auth.service';
+import { log } from 'console';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatSelectModule, CommonModule,ReactiveFormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm:FormGroup;
+constructor(private formBuilder:FormBuilder, private authService: AuthService){}
+  ngOnInit(): void {
+   this.createLoginForm();
+  }
+
+  createLoginForm(){
+    this.loginForm = this.formBuilder.group({
+      email: ["",Validators.required],
+      password: ["",Validators.required],
+
+    })
+  }
+
+  login(){
+    if(this.loginForm.valid)
+    {
+      console.log(this.loginForm.value);
+      let loginModel = Object.assign({},this.loginForm.value)
+
+      this.authService.login(loginModel).subscribe(data => {
+        console.log(data)
+      })
+    }
+  }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginModel } from '../models/loginModel';
+import { TokenModel } from '../models/tokenModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiUrl = 'https://localhost:7131/api/Auth/';
+  constructor(private httpClient:HttpClient) { }
+
+  login(loginModel:LoginModel) {
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl+"login",loginModel)
+  }
+
+  isAuthenticated(){
+    if(localStorage.getItem("token")){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
